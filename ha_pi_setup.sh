572e3d27042d4b946fc20cfcfa5a8655d3f3f0a8
ohall <ohall@grassroots-tech.com>
2021-11-27 20:47:40 -0500

    zwave-config:/usr/src/app/store

--- ha_pi_setup.sh
+++ ha_pi_setup.sh
@@ -1,34 +1,22 @@
-#HA Pi setup
-# passwd
+# HA Pi setup
+
+# Update password
+passwd
+
+# Install depenndencies
 sudo apt update
-sudo apt install git
+sudo apt install -y python3 python3-pip libffi-dev git
 
+# Install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
+export LC_CTYPE=en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && sudo sh get-docker.sh
+sudo groupadd docker
 sudo usermod -aG docker pi
 sudo chmod 666 /var/run/docker.sock
-
-sudo apt update
-sudo apt install -y python3 python3-pip libffi-dev
 sudo pip3 install docker-compose
 
+# Pull docker-compose setup and run
+scp -r .ssh pi@raspberrypi.local:/home/pi/
 git clone git@github.com:ohall/ha.git
-
 cd ha
-
-sudo cp home-assistant.service /etc/systemd/system/home-assistant.service
-
-systemctl enable home-assistant.service
-systemctl enable docker
-
-## ZWave server
-# nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-nvm install 16
-npm init
-npm i @zwave-js/server
-npm i -g ts-node
-npm i --save-dev @types/ws @types/triple-beam @types/minimist
-
-./node_modules/.bin/ts-node node_modules/@zwave-js/server/src/bin/server.ts /dev/tty0
+git pull && docker-compose up -d
